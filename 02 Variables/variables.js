f7d4fc51feea77a6c5ce488634a58127c45ec468
aratisahoo-arati014 <sahuarati557@gamil.com>
2024-07-24 20:02:44 +0530

    update variables file

---
+++ 02 Variables/variables.js
@@ -0,0 +1,32 @@
+//types of variable
+//let
+let a = 10
+let name='javascript';
+ name='Arati';
+ name='siba';
+ console.log(name)
+ console.log(a);
+
+
+//var 
+var age=34;
+var age=20;
+console.log(age)
+//const
+const salary=50000;
+//const salary=40000;
+console.log(salary)
+
+//object
+const student={
+    fullName :"Rahul Kumar",
+    age : 20,
+    cgpa : 7.8,
+    isPass:true 
+}
+
+student["age"]=student["age"]+1;
+
+student["fullName"]="Rasmi sahu"
+console.log(student["age"]);
+console.log(student.fullName);
